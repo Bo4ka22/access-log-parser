101ea862c9a627dcd8abc2c5d6c9dafabd29180e
Stanislav Prygunov <kzk0n@yandex.ru>
2024-03-06 19:27:53 +0300

    Добавлен цикл с запросом пути к файлу и проверками существует ли такой файл, является ли введённый адрес папкой.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +6,24 @@ public class Main {
         System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
+        int counter =0;
+
+        while (true){
+            System.out.println("Введите путь к файлу для парсинга и нажмите <Enter>: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExist) {
+                System.out.println("Указанный файл не существует"); continue;
+            }
+            if (isDirectory) {
+                System.out.println("Указан путь к папке, а не к файлу"); continue;
+            }
+            counter++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл № " + counter);
+        }
     }
 }
