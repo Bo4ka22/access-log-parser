260d3d17e5dfe244aa8965701e150c25c13c0b1f
Stanislav Prygunov <kzk0n@yandex.ru>
2024-04-03 18:30:24 +0300

    Добавлен функционал чтения файла с поиском самой длинной и короткой строки, а так же общего количества строк

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -6,9 +9,10 @@ public class Main {
         System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
-        int counter =0;
+        int counter = 0;
 
-        while (true){
+        while (true) {
+            //Блок для проверки наличия указанного файла
             System.out.println("Введите путь к файлу для парсинга и нажмите <Enter>: ");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
@@ -24,6 +28,37 @@ public class Main {
             counter++;
             System.out.println("Путь указан верно");
             System.out.println("Это файл № " + counter);
+
+            //Блок для чтения строк указанного файла и поиска самой длинной и короткой строки, а так же общего количества строк
+            int linesCounter = 0;
+            int longestLine = 0;
+            ArrayList<Integer> tmp = new ArrayList<>();
+
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader =
+                        new BufferedReader(fileReader);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    linesCounter++;
+                    int length = line.length();
+                    if (length > 1024) throw new RuntimeException("Строка не должна содержать более 1024 символа");
+                    if (length > longestLine) longestLine = length;
+                    tmp.add(length);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            int shortestLine = tmp.get(0);
+            for (int i = 1; i < tmp.size(); i++) {
+                if (shortestLine > tmp.get(i)) shortestLine = tmp.get(i);
+            }
+            System.out.println("Всего строк в файле: " + linesCounter);
+            System.out.println("Самая длинная строка содержит символов: " + longestLine);
+            System.out.println("Самая короткая строка содержит символов : " + shortestLine);
         }
+
+
+
     }
 }
